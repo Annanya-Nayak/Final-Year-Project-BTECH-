82577a202fdb9d08d5e7d32fd8fe9fdfb7210b94
Annanya-Nayak <annanyanayak@gmail.com>
2026-08-07 19:24:52 +0530

    Delete plots directory

--- plots/main.py
+++
@@ -1,7 +0,0 @@
-@app.get('/policy/history')
-async def policy_history():
-    return {
-        'rewards': policy_engine.reward_history,
-        'epsilons': policy_engine.epsilon_history,
-        'decisions': policy_engine.decision_log
-    }
